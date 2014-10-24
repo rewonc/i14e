85b1b550293bbf158421b2efb825c09ddac166ec
rewonc <rewonc@gmail.com>
2014-10-23 18:40:02 -0700

    add mongodb

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [com.novemberain/monger "2.0.0"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler i14e.core.handler/app}
   :profiles
--- src/i14e/core/handler.clj
+++ src/i14e/core/handler.clj
@@ -1,11 +1,30 @@
 (ns i14e.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [monger.core :as mg]
+            [monger.collection :as mc])
+  (:import [com.mongodb MongoOptions ServerAddress]))
+
+(defn cn 
+  "Returns a connection to the specified DB"
+  ([] (mg/get-db (mg/connect) "test"))
+  ([db] (mg/get-db (mg/connect) db)))
+
+(defn insert [cn coll first_name last_name]
+    (mc/insert cn coll {:first_name first_name  :last_name last_name})
+    (str first_name " " last_name))
+        
+(defn retrieve [cn, coll]
+    (print-str (mc/find-maps cn coll))) 
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/get" [] (str (retrieve (cn) "documents")))
+  (GET "/insert/:first_name/:last_name" 
+    [first_name last_name] (insert (cn) "documents" first_name last_name))
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
