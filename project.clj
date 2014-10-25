6ed59edb9bac4f5aec86b57bd05f60961bf4dfce
rewonc <rewonc@gmail.com>
2014-10-24 23:44:43 -0700

    hiccup templating and flat ui

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
                  [ring/ring-defaults "0.1.2"]
-                 [com.novemberain/monger "2.0.0"]]
+                 [com.novemberain/monger "2.0.0"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler i14e.core.handler/app}
   :profiles
--- src/i14e/core/handler.clj
+++ src/i14e/core/handler.clj
@@ -3,7 +3,8 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [monger.core :as mg]
-            [monger.collection :as mc])
+            [monger.collection :as mc]
+            [hiccup.page :as hp])
   (:import [com.mongodb MongoOptions ServerAddress]))
 
 (defn env [] 
@@ -22,8 +23,31 @@
 (defn retrieve [cn, coll]
     (print-str (mc/find-maps cn coll))) 
 
+(defn template [body] 
+  (hp/html5 
+    {:lang "en"}    
+    [:meta {:charset "utf-8"}]
+    [:title "i14n"]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+    [:link {:href "css/vendor/bootstrap.min.css" :rel "stylesheet"}]
+    [:link {:href "css/flat-ui.css" :rel "stylesheet"}]
+    [:link {:href "img/favicon.ico" :rel "shortcut icon"}]
+    [:head "<!--[if lt IE 9]>
+      <script src=\"js/vendor/html5shiv.js\"></script>
+      <script src=\"js/vendor/respond.min.js\"></script>
+    <![endif]-->"]
+    [:div.container
+      [:p "This is some kind of intro"]
+      [:h2 "hello world"] 
+      body
+     ]
+
+    (hp/include-js "js/vendor/jquery.min.js")
+    (hp/include-js "js/flat-ui.min.js")
+   ))
+
 (defroutes app-routes
-  (GET "/" [] (str (env)))
+  (GET "/" [] (template [:p "body"]))
   (GET "/get" [] (str (retrieve (cn) "documents")))
   (GET "/insert/:first_name/:last_name" 
     [first_name last_name] (insert (cn) "documents" first_name last_name))
