03b1755f8aa285d73f931c214cc24935cff55122
rewonc <rewonc@gmail.com>
2014-10-30 09:32:13 -0700

    remove excess code from routing page

--- src/i14e/core/handler.clj
+++ src/i14e/core/handler.clj
@@ -2,46 +2,20 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [monger.core :as mg]
-            [monger.collection :as mc]
-            [oauth.client :as oauth]
-            [clj-http.client :as client]
             [i14e.core.template :as template]
-            [i14e.core.twitter-data :as tdata]
             [i14e.core.twitter-request :as twitter] ) )
-(defn env [] 
-  (get (System/getenv) "MONGOHQ_URL"))
-
-
-(defn cn []
-  "Returns a connection to the specified DB"
-  (if (env) 
-    ( (mg/connect-via-uri (env)) :db )
-    (mg/get-db (mg/connect) "test") ))
-
-(defn insert [cn coll first_name last_name]
-    (mc/insert cn coll {:first_name first_name  :last_name last_name})
-    (str first_name " " last_name))
-        
-(defn retrieve [cn, coll]
-    (print-str (mc/find-maps cn coll))) 
 
 (defroutes app-routes
-  (GET "/" [] (template/summary [:div.counts
+  (GET "/hello" [] (template/summary [:div.counts
     [:h5 "Some real-time queries of data"]
     [:p (str "Count one cursored result for followers of pmarca: " (-> (twitter/followers-of "pmarca" "958968890" ) 
       (get "ids") 
       (count) ))]
     ] ))
-  (GET "/get" [] (str (retrieve (cn) "tokens")))
-  (GET "/hello" [] (str (twitter/uri)))
+  (GET "/" [] (str (twitter/uri)))
   (GET "/req" [] (str (twitter/followers-of "pmarca" "958968890" )))
-
   (GET "/session" {session :session} 
     {:status 200 :body (str "session txt" session) :headers {"Content-Type" "text/html"} :session (assoc session :hello "world") })
-  (GET "/following" [] (str (twitter/following twitter/temporary-token)))
-  (GET "/insert/:first_name/:last_name" 
-    [first_name last_name] (insert (cn) "documents" first_name last_name))
   (GET "/auth/callback" {params :query-params session :session} 
     (let [auth (twitter/access-token-response (get params "oauth_token") (get params "oauth_verifier"))]
       {:status 200 :body (str "user saved to session: " (:screen_name auth)) :headers {"Content-Type" "text/html"} :session (assoc session :user_id (:user_id auth) ) }))
